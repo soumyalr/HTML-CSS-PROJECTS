b44d264909a5da0a605f1f9f34a7f3edeedfc4ee
Soumya Bhakta <22a91a05g3aec.edu.in>
2024-08-04 19:03:36 +0530

    new projects

--- javascript/first.js
+++
@@ -1,4 +0,0 @@
-let str=`this is STRtring value`;
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str);
---
+++ js/script.js
@@ -0,0 +1,2 @@
+let button = document.querySelector("Button");
+let k = button onclick=console.log("button was clicked");
